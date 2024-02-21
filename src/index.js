a4b6090581f2d237ebbe460c84268f92441d6146
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2024-02-21 18:37:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import {persistor, store} from './store'
 import { PersistGate } from 'redux-persist/integration/react';
 
-export const ROOT_URL = 'http://localhost:3333'
+export const ROOT_URL = 'https://git-hub-backend.onrender.com'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
