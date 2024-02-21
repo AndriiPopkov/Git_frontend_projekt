0f47937c6dfc27f71e03f6e82ff4ad12e88e3ba8
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2024-02-21 10:28:54 +0100

    Add files via upload

---
+++ src/components/BasketElement/BasketElement.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { ROOT_URL } from '../..'
+import s from './BasketElement.module.css'
+import minus from './media/minus.png'
+import plus from './media/plus.png'
+import { useDispatch, useSelector } from 'react-redux'
+import { addProduct, deleteItem, deleteProduct } from '../../store/basketProductsSlice'
+
+export default function BasketElement({ id, dataProduct, count }) {
+
+    const products = useSelector(store => store.productsbasket)
+
+    const dispatch = useDispatch()
+
+    const countProduct = 1
+
+    const {price, discont_price} = dataProduct
+    // console.log(dataProduct);
+
+    const addCountProduct = () => {
+        dispatch(addProduct({ product: dataProduct, countProduct }));
+    }
+    const newCount = count-1
+
+    const delCountProduct = () => {
+        dispatch(deleteProduct({ product: dataProduct, countProduct, newCount }));
+    }
+
+    const handler = (e) => {
+        e.stopPropagation();
+        dispatch(deleteItem({id, dataProduct, count}))
+    }
+
+    return (
+        <div className={s.basket_element}>
+            <span onClick={handler}>x</span>
+            <img src={ROOT_URL + dataProduct.image} alt="" />
+            <div>
+                <p>{dataProduct.title}</p>
+                <div className={s.count_price}>
+                    <div className={s.counter}>
+                        <button 
+                            onClick={delCountProduct} 
+                            className={s.p_m}>
+                            <img src={minus} alt="minus" />
+                        </button>
+                        {count}
+                        <button
+                            onClick={addCountProduct}
+                            className={s.p_m}>
+                            <img src={plus} alt="plus" />
+                        </button>
+                    </div>
+                    <div className={s.price}>
+                        <p>{`$${discont_price === null ? price:discont_price}`}</p>
+                        <p>{discont_price === null ? '':`$${price}`}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/CategoriesPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AllCategories from "../components/AllCategories/AllCategories";
+import BreadCrumbs from "../components/Breadcrums/BreadCrumbs";
+
+export default function CategoriesPage() {
+
+  return (
+    <section className="wrapper">
+      <AllCategories/>
+    </section>
+  );
+}
+
